088cf137258fec9e4261d67f79df50752ed982cf
Ganeshrockz <ganeshrockz@github.com>
2021-10-25 20:34:55 +0530

    Updates

---
+++ precache-manifest.b1ea8302d4bc5880d351ecb43cac2629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "540bb455dac92dc1ec20",
+    "url": "/wt20-21/static/css/main.31e8e36b.chunk.css"
+  },
+  {
+    "revision": "540bb455dac92dc1ec20",
+    "url": "/wt20-21/static/js/main.540bb455.chunk.js"
+  },
+  {
+    "revision": "9852730fec0d504bb803",
+    "url": "/wt20-21/static/js/runtime~main.9852730f.js"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/wt20-21/static/css/2.801b82c9.chunk.css"
+  },
+  {
+    "revision": "0be92d95ed26d5569b46",
+    "url": "/wt20-21/static/js/2.0be92d95.chunk.js"
+  },
+  {
+    "revision": "17fb822e0f146667dc4d7c1c482600ba",
+    "url": "/wt20-21/index.html"
+  }
+];
